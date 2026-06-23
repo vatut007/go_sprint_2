02062ee4c8a4b2078ff2e0fd0b4cdf86642e0ec3
Ilya <sov16@ya.ru>
2026-06-23 17:26:36 +0300

    1

--- lesson_1/4/main.go
+++ lesson_1/4/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 type NetAddress struct {
@@ -10,8 +13,23 @@ type NetAddress struct {
 	Port int
 }
 
-// допишите код реализации методов интерфейса
-// ...
+func (a NetAddress) String() string {
+	return fmt.Sprintf("%s:%d", a.Host, a.Port)
+}
+
+func (a *NetAddress) Set(s string) error {
+	hp := strings.Split(s, ":")
+	if len(hp) != 2 {
+		return errors.New("Need address in a form host:port")
+	}
+	port, err := strconv.Atoi(hp[1])
+	if err != nil {
+		return err
+	}
+	a.Host = hp[0]
+	a.Port = port
+	return nil
+}
 
 func main() {
 	addr := new(NetAddress)
