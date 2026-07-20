6064b84b34ff6f3bf2b2e8b58f556922c171395c
Ilya <sov16@ya.ru>
2026-07-20 16:50:48 +0300

    new lesson

---
+++ sprint3/lesson_2/7/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+)
+
+// SendData — структура для кодирования.
+type SendData struct {
+	Value   int
+	Balance *float64
+	Name    string
+	private int
+}
+
+// GetData — структура для декодирования.
+type GetData struct {
+	Name    string
+	Balance float64
+	Ext     []byte
+	value   int
+}
+
+func main() {
+	// создаём исходный объект
+	floatValue := 50.0
+	data := SendData{Value: 100, Balance: &floatValue, Name: "Василий Кузнецов",
+		private: 1} // создаём хранилище байт и энкодер/декодер
+	var buffer bytes.Buffer
+	// кодирование
+	if err := gob.NewEncoder(&buffer).Encode(data); err != nil {
+		panic(err)
+	}
+	// сейчас buffer содержит data в формате gob
+
+	// декодирование будет в переменную типа GetData
+	var out GetData
+	if err := gob.NewDecoder(&buffer).Decode(&out); err != nil {
+		panic(err)
+	}
+	fmt.Printf("out: %+v\n", out)
+}
